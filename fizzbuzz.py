4034ea991cff9afee4718040d4dd0a18e9824e08
funera1 <60760935+funera1@users.noreply.github.com>
2024-05-31 14:01:20 +0900

    Add python code and CI

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+def fizzbuz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return 'FizzBuzz'
+    if num % 3 == 0:
+        return 'Fizz'
+    if num % 5 == 0:
+        return 'Buzz'
+
+    return num
